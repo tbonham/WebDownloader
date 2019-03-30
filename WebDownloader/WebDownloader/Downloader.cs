b12deed9a9e6556e46c7aa7c291e905cedfc3cc7
Thomas Bonham <bonham@thomasbonham.com>
2019-03-30 15:51:06 -0700

    Adding method for downloading a stream of data.

---
+++ WebDownloader/WebDownloader/Downloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebDownloader
+{
+    public class Downloader
+    {
+        public async static Task<String> ResultsData(String Url)
+        {
+            Task<String> data = null;
+            WebClient client = new WebClient();
+            client.DownloadStringAsync(new Uri(Url), data);
+            return await data;
+        }
+    }
+}
